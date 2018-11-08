9bc72a7d04ee4c919f9a53c2a9bba033c593e4be
Pietro Alovisi <alovisi@kth.se>
2018-11-08 12:05:59 +0100

    lab1

---
+++ lab1/gaussfft.m
@@ -0,0 +1,16 @@
+function out = gaussfft(img,t)
+
+    %[X,Y] = meshgrid(1:size(img,1),1:size(img,1));
+
+    [X,Y] = meshgrid(-size(img,1)/2:(size(img,1)/2-1),-size(img,1)/2:(size(img,1)/2-1));
+    Z = mvnpdf([X(:) Y(:)],0,eye(2,2)*sqrt(t));
+    
+    Z = reshape(Z,size(X,1),size(X,1));
+    figure();
+    surf(X,Y,Z);
+    Zhat = fft2(Z);
+    surf(X,Y,abs(Zhat));
+    %surf(X,Y,abs(fft2(img)));
+    out = ifftshift(abs(ifft2(Zhat.* fft2(img))));
+end
+
