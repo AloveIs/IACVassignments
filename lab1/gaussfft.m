872540cbf5f7c3a891f2e731ef93ffa00dd45d0f
Pietro Alovisi <alovisi@kth.se>
2018-11-13 18:58:42 +0100

    changed one value in the gaussfft

--- lab1/gaussfft.m
+++ lab1/gaussfft.m
@@ -3,7 +3,7 @@ function out = gaussfft(img,t)
     %[X,Y] = meshgrid(1:size(img,1),1:size(img,1));
 
     [X,Y] = meshgrid(-size(img,1)/2:(size(img,1)/2-1),-size(img,1)/2:(size(img,1)/2-1));
-    Z = mvnpdf([X(:) Y(:)],0,eye(2,2)*sqrt(t));
+    Z = mvnpdf([X(:) Y(:)],0,eye(2,2)*t);
     
     Z = reshape(Z,size(X,1),size(X,1));
     %figure();
